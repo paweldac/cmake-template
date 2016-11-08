d08c05dbc5e6deaa9f3041b37ec4d9f777dc87f7
Pawel Dac <pawel.dac@gmail.com>
2016-11-08 20:41:16 +0100

    change: sucessful integration with travis-ci.

--- cmake/clang/build-types.cmake
+++ cmake/clang/build-types.cmake
@@ -5,17 +5,17 @@ endif()
 set(CLANG_BUILD_TYPES_INCLUDED true)
 #######################
 
-set(BUILD_TYPE "DEBUG" CACHE STRING "Build type, choose from DEBUG. RELEASE and MIN_RELEASE")
+set(BUILD_TYPE "Debug" CACHE STRING "Build type, choose from Debug. Release and MinRelease")
 
-if(BUILD_TYPE STREQUAL "DEBUG")
+if(BUILD_TYPE STREQUAL "Debug")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ggdb -g -gdwarf-2")
-elseif(BUILD_TYPE STREQUAL "RELEASE")
+elseif(BUILD_TYPE STREQUAL "Release")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2")
-elseif(BUILD_TYPE STREQUAL "MIN_RELEASE")
+elseif(BUILD_TYPE STREQUAL "MinRelease")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os -fdata-sections -ffunction-sections")
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
 else()
-    message(FATAL_ERROR "Wrong BUILD_TYPE. Should be DEBUG, RELEASE or MIN_RELEASE.")
+    message(FATAL_ERROR "Wrong BUILD_TYPE. Should be Debug, Release or MinRelease.")
 endif()
 
 message(STATUS "Build type is ${BUILD_TYPE}")
