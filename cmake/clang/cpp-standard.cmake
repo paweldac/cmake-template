523feaa6df137d763f867d90e18b14aa303b4b27
Pawel Dac <pawel.dac@gmail.com>
2017-04-03 19:33:02 +0200

    change: manual --std compilation flag settings replaced with cmake_cxx_standard variable

--- cmake/clang/cpp-standard.cmake
+++ cmake/clang/cpp-standard.cmake
@@ -5,18 +5,20 @@ endif()
 set(CLANG_CPP_STANDARD_INCLUDED true)
 #######################
 
-set(CPP_STANDARD "C++11" CACHE STRING "C++ standard, choose from C++03, C++11, C++14 and C++17")
+set(CMAKE_CXX_EXTENSIONS OFF)
 
-if(CPP_STANDARD STREQUAL "C++03")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++03")
+set(CPP_STANDARD "C++11" CACHE STRING "C++ standard, choose from C++98, C++11, C++14 and C++17")
+
+if(CPP_STANDARD STREQUAL "C++98")
+    set(CMAKE_CXX_STANDARD 98)
 elseif(CPP_STANDARD STREQUAL "C++11")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    set(CMAKE_CXX_STANDARD 11)
 elseif(CPP_STANDARD STREQUAL "C++14")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+    set(CMAKE_CXX_STANDARD 14)
 elseif(CPP_STANDARD STREQUAL "C++17")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
 else()
-    message(FATAL_ERROR "Wrong C++ Standard. Choose from C++03, C++11, C++14 and C++17")
+    message(FATAL_ERROR "Wrong C++ Standard. Choose from C++98, C++11, C++14 and C++17")
 endif()
 
-message(STATUS "C++ standard is ${CPP_STANDARD}")
+message(STATUS "C++ standard is set to: ${CPP_STANDARD}")
