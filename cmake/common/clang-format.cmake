f995440f95525bea0c89bede43ea2a847e7083a3
Pawel Dac <pawel.dac@gmail.com>
2017-04-03 20:03:19 +0200

    change: exclude directory with testing framework

--- cmake/common/clang-format.cmake
+++ cmake/common/clang-format.cmake
@@ -6,7 +6,7 @@ set(CLANG_FORMAT_INCLUDED true)
 #######################
 
 add_custom_target(
-        format
-        COMMAND find src tests -iname *.h -o -iname *.hpp -o -iname *.c -iname *.cpp | xargs clang-format -style=file -i
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    format
+    COMMAND find src tests -type f '\(' -iname *.h -o -iname *.hpp -o -iname *.c -o -iname *.cpp '\)' -not -path "*/tests/tools/*"  | xargs clang-format -style=file -i
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 )
