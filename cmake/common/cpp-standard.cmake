24e47695e795b65871e78dd06bf37541c846a480
Pawel Dac <pawel.dac@gmail.com>
2018-03-20 19:30:43 +0100

    improved cpp standard selection
    added dropdown selection list with standard versions for ccmake/cmake-gui

--- cmake/common/cpp-standard.cmake
+++ cmake/common/cpp-standard.cmake
@@ -1,13 +1,19 @@
 ##### pragma once #####
-if(CLANG_CPP_STANDARD_INCLUDED)
+if(CPP_STANDARD_INCLUDED)
     return()
 endif()
-set(CLANG_CPP_STANDARD_INCLUDED true)
+set(CPP_STANDARD_INCLUDED true)
 #######################
 
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-set(CPP_STANDARD "C++11" CACHE STRING "C++ standard, choose from C++98, C++11, C++14 and C++17")
+set(CPP_STANDARD "C++14" CACHE STRING
+    "C++ standard, choose from C++98, C++11, C++14 and C++17." FORCE
+)
+set_property(CACHE CPP_STANDARD PROPERTY STRINGS
+    "C++98" "C++11" "C++14" "C++17"
+)
 
 if(CPP_STANDARD STREQUAL "C++98")
     set(CMAKE_CXX_STANDARD 98)
@@ -16,7 +22,7 @@ elseif(CPP_STANDARD STREQUAL "C++11")
 elseif(CPP_STANDARD STREQUAL "C++14")
     set(CMAKE_CXX_STANDARD 14)
 elseif(CPP_STANDARD STREQUAL "C++17")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
+    set(CMAKE_CXX_STANDARD 17)
 else()
     message(FATAL_ERROR "Wrong C++ Standard. Choose from C++98, C++11, C++14 and C++17")
 endif()
