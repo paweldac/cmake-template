21d89043c3bd8a41f70617d5985c026390acfcaf
Pawel Dac <pawel.dac@gmail.com>
2020-07-22 23:15:47 +0200

    Added [leak|memory|undefinedbehavior] sanitizer as possible MEMORY_SANITIZER options
    changes:
    - added leak sanitizer to supported MEMORY_SANITIZERs
    - added clang10/gcc10 with leak sanitizer enabled builds
    - added memory sanitizer to supported MEMORY_SANITIZERs
    - added instrumented llvm version for memory sanitizer build
    - notice user when MemorySanitizer is enabled with compiler outside of clang family
    - added clang10 with memory sanitizer enabled build
    - added undefinedbehavior sanitizer to supported MEMORY_SANITIZERs
    - added clang10/gcc10 with undefinedbehavior sanitizer enabled builds

--- cmake/memory-sanitizers.cmake
+++ cmake/memory-sanitizers.cmake
@@ -5,13 +5,28 @@ include(colorful-message)
 set(VALGRIND "Valgrind")
 set(ASAN "AddressSanitizer")
 set(TSAN "ThreadSanitizer")
+set(LSAN "LeakSanitizer")
+set(MSAN "MemorySanitizer")
+set(UBSAN "UndefinedBehaviorSanitizer")
 
 if(NOT MEMORY_SANITIZER)
   set(MEMORY_SANITIZER
       "None"
-      CACHE STRING "Memory sanitizer for build, choose from ${VALGRIND}, ${ASAN}, ${TSAN} and None"
-            FORCE)
-  set_property(CACHE MEMORY_SANITIZER PROPERTY STRINGS ${VALGRIND} ${ASAN} ${TSAN} "None")
+      CACHE
+        STRING
+        "Memory sanitizer for build, choose from ${VALGRIND}, ${ASAN}, ${TSAN}, ${LSAN}, ${MSAN}, ${UBSAN} and None"
+        FORCE)
+
+  set_property(
+    CACHE MEMORY_SANITIZER
+    PROPERTY STRINGS
+             ${VALGRIND}
+             ${ASAN}
+             ${TSAN}
+             ${LSAN}
+             ${MSAN}
+             ${UBSAN}
+             "None")
 endif()
 
 if(${MEMORY_SANITIZER} MATCHES ${VALGRIND})
@@ -38,6 +53,45 @@ if(${MEMORY_SANITIZER} MATCHES ${TSAN})
   set(MEMORYCHECK_SANITIZER_OPTIONS "verbosity=1:history_size=7")
   add_compile_options("$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=thread;>")
   add_link_options("$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=thread>")
+endif()
+
+if(${MEMORY_SANITIZER} MATCHES ${LSAN})
+  set(MEMORYCHECK_TYPE LeakSanitizer)
+  set(MEMORYCHECK_SANITIZER_OPTIONS "verbosity=1")
+  add_compile_options(
+    "$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=leak;-fno-omit-frame-pointer;-fstack-protector-all>"
+  )
+  add_link_options("$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=leak>")
+endif()
+
+if(${MEMORY_SANITIZER} MATCHES ${MSAN})
+  if(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
+    set(MEMORYCHECK_TYPE MemorySanitizer)
+    set(MEMORYCHECK_SANITIZER_OPTIONS "verbosity=1:poison_in_dtor=1:history_size=7")
+    add_compile_options(
+      "$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-fsanitize=memory;-fsanitize-memory-use-after-dtor;-fsanitize-memory-track-origins;-fno-omit-frame-pointer>"
+    )
+    add_link_options("$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-fsanitize=memory>")
+  else()
+    cmessage(
+      NOTICE
+      "MemorySanitizer cannot be enabled. Compiler(${CMAKE_CXX_COMPILER_ID}) doesn't support `-fsanitize=memory`"
+    )
+    set(${MEMORY_SANITIZER} "None")
+  endif()
+endif()
+
+if(${MEMORY_SANITIZER} MATCHES ${UBSAN})
+  set(MEMORYCHECK_TYPE UndefinedBehaviorSanitizer)
+  set(MEMORYCHECK_SANITIZER_OPTIONS "verbosity=1:print_stacktrace=1")
+  add_compile_options(
+    "$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=undefined;-fsanitize=float-divide-by-zero>"
+  )
+  add_compile_options("$<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fsanitize=float-cast-overflow>")
+  add_compile_options(
+    "$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-fsanitize=unsigned-integer-overflow;-fsanitize=implicit-conversion;-fsanitize=local-bounds;-fsanitize=nullability>"
+  )
+  add_link_options("$<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang,GNU>:-fsanitize=undefined>")
 endif()
 
 if(NOT ${MEMORY_SANITIZER} MATCHES "None")
